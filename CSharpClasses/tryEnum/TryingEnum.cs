bca4487854ca9603dd15f8502fb6b48aa7fa60ed
Teja Linux <raviteja.yella@gmail.com>
2021-08-17 16:56:09 +0530

    added comments about value and reference types

--- CSharpClasses/tryEnum/TryingEnum.cs
+++ CSharpClasses/tryEnum/TryingEnum.cs
@@ -1,3 +1,11 @@
+// Structures - Value Types - Primitives types (int, char, float, bool) and  custom Structures
+// Classes - Reference Types - Non-Primitive types (classes, structures, arrays, strings)
+
+// Value Types and Reference types. 
+// Value types are stored in Stack Memory
+// Reference types are stored in Heap Memory
+// Reference of the classes stored in Heap Memory are stored in Stack Memory as the named variable
+
 using System;
 
 namespace CSharpClasses.tryEnum
