45fee0d31f4731035a334fde3e8410a4548aa266
Teja Linux <raviteja.yella@gmail.com>
2021-08-17 18:49:45 +0530

    challenges folder

---
+++ challenges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace challenges
+{
+    public class Conditionals
+    {
+        public void Question1()
+        {
+            System.Console.WriteLine("Enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            string check = (userNumber > 0 && userNumber < 10)? "Valid":"Invalid";
+            System.Console.WriteLine(check);
+        }
+    }
+    static void Main(string[] args)
+    {
+    }
+}
