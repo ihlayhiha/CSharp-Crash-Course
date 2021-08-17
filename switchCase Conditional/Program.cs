217170d5b1ecca39477d534158d0559f7780bf36
Teja Linux <raviteja.yella@gmail.com>
2021-08-17 18:39:39 +0530

    Added my solutions to challenge questions

--- switchCase Conditional/Program.cs
+++ switchCase Conditional/Program.cs
@@ -24,6 +24,64 @@ namespace switchCase_Conditional
                     System.Console.WriteLine("I don't understand this season");
                     break;
             }
+
+
+            System.Console.WriteLine("Input a number between 1 and 10");
+            string user = Console.ReadLine();
+
+            // try
+            // {
+            //     int userInput = Convert.ToInt32(user);
+
+            // }
+            // catch (System.Exception)
+            // {
+            //     System.Console.WriteLine("Invalid");
+            // }
+            
+            int userInput = Convert.ToInt32(user);
+
+            var result = (userInput > 0 && userInput < 10)? "Valid!" : "Invalid"; 
+            System.Console.WriteLine(result);
+
+
+            System.Console.WriteLine("Enter the first number");
+            string str1 = Console.ReadLine();
+            System.Console.WriteLine("Enter the second number");
+            string str2 = Console.ReadLine();
+
+            string max = (Convert.ToInt16(str1) > Convert.ToInt16(str2))? str1 : str2;
+            System.Console.WriteLine(max);
+
+            System.Console.WriteLine("Enter Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Enter Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            
+            string check = (width > height)? "Landscape":"Portrait";
+            System.Console.WriteLine(check);
+
+
+            // ------------ TRAFFIC CAMERA
+            System.Console.WriteLine("Enter speed limit: ");
+            int speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Enter your car speed: ");
+            int carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+            {
+                System.Console.WriteLine("OK");
+            }
+            else
+            {
+                int demerit = (carSpeed - speedLimit) / 5;
+                System.Console.WriteLine(demerit);
+                if (demerit > 12)
+                    System.Console.WriteLine("License Suspended");
+            }
+                
+
         }
     }
 }
---
+++ switchCase Conditional/challenges.cs
@@ -0,0 +1,6 @@
+// using System;
+
+// namespace switchCase_Conditional
+// {
+
+// }
