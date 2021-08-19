2ab2ed0eef900e5850567ac6126efdfec22d24d7
Teja Linux <raviteja.yella@gmail.com>
2021-08-19 19:12:25 +0530

    basic composition

---
+++ Composition/DBMigrator.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Composition
+{
+    public class DBMigrator
+    {
+        private readonly Logger _logger;
+
+        public DBMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We are migrating");
+        }
+    }
+}
